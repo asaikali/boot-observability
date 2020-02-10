9260619223531fc1ae5dbceb15cd4176097cb182
Adib Saikali <asaikali@gmail.com>
2020-02-10 06:22:25 -0500

    Add metrics placeholder project

---
+++ multi-dimensional-metrics/src/main/java/com/example/MultiDimensionalMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDimensionalMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDimensionalMetricsApplication.class, args);
+	}
+
+}
